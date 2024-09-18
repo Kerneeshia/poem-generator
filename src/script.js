405063d7b23e2a2e65f23bd36a063ba237c9bcbc
kerneeshia <kerneeshia.baaitjies@gmail.com>
2024-09-18 17:28:40 +0100

    finishing touches, adding links and additional style elements

--- src/script.js
+++ src/script.js
@@ -14,9 +14,12 @@ function getPoem(event){
 
 let apiKey ="a3460f11d3801ff9feb7c65d8btoaadb";
 let userInstruction = document.querySelector("#user-instruction");
-let context="you are a creative and romantic poet that writes the most beautiful poems about every topic in life or abstract. Make sure to adhere to user prompts. Make sure to put the title of the poem in its own line at the top of the poem and the poem should consist of a maximum of three stanzas";
+let context="you are a creative and romantic poet that writes the most beautiful poems about every topic in life or abstract. Make sure to adhere to user prompts. Make sure to put the title of the poem in its own line at the top of the poem and the poem should consist of a maximum of two stanzas and a line break between each stanza ";
 let prompt=`Generate a beautiful poem based on the ${userInstruction.value} entered`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+let poemElement=document.querySelector("#poem");
+poemElement.innerHTML=`<div class="generating"> Writing</div>`;
 
 axios.get(apiUrl).then(displayPoem);
 
