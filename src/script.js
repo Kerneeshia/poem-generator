65c97d332575d62e7f91e3a1f2a7052f11016ec9
kerneeshia <kerneeshia.baaitjies@gmail.com>
2024-09-18 15:14:25 +0100

    context updated

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
-function displayPoem(response){
+function displayPoem(response) {
 
     new Typewriter("#poem", {
         strings: response.data.answer,
-        autostart: true,
+        autoStart: true,
         delay: 1,
         cursor:"",
 
@@ -14,7 +14,7 @@ function getPoem(event){
 
 let apiKey ="a3460f11d3801ff9feb7c65d8btoaadb";
 let userInstruction = document.querySelector("#user-instruction");
-let context="you are a creative and romantic poet that writes the most beautiful poems about every topic in life or abstract. Make sure to adhere to user prompts.";
+let context="you are a creative and romantic poet that writes the most beautiful poems about every topic in life or abstract. Make sure to adhere to user prompts. Make sure to put the title of the poem in its own line at the top of the poem and the poem should consist of a maximum of three stanzas";
 let prompt=`Generate a beautiful poem based on the ${userInstruction.value} entered`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
