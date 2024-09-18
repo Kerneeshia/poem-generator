ff897b94c96aaba348d7422822110a73caece8a6
kerneeshia <kerneeshia.baaitjies@gmail.com>
2024-09-18 14:42:38 +0100

    added api integration

--- src/script.js
+++ src/script.js
@@ -1,16 +1,21 @@
 function displayPoem(response){
-    let poemElement = document.querySelector("#poem");
-    // poemElement.innerHTML=response.data.answer;
 
-alert(response.data.answer);
+    new Typewriter("#poem", {
+        strings: response.data.answer,
+        autostart: true,
+        delay: 1,
+        cursor:"",
+
+    });
 }
 
 function getPoem(event){
     event.preventDefault();
 
 let apiKey ="a3460f11d3801ff9feb7c65d8btoaadb";
-let context="you are an AI assistant that generates the most beautiful poems about every topic in life based on the request of the user";
-let prompt="Generate a beautiful poem based on the requested topic entered by the user";
+let userInstruction = document.querySelector("#user-instruction");
+let context="you are a creative and romantic poet that writes the most beautiful poems about every topic in life or abstract. Make sure to adhere to user prompts.";
+let prompt=`Generate a beautiful poem based on the ${userInstruction.value} entered`;
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(displayPoem);
